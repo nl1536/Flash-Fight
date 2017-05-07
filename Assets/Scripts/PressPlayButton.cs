7fad794251233ae20d420308138818a862892ce3
Unknown <kelseyhu2425@gmail.com>
2017-05-07 13:13:46 -0400

    switch scene, built both scenes, add button sprites

---
+++ Assets/Scripts/PressPlayButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressPlayButton : MonoBehaviour
+{
+
+	private Vector3 SelectedPos;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			SelectedPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+									   Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+			RaycastHit2D hit = Physics2D.Raycast(SelectedPos, Vector2.zero, 0f);
+
+			if (hit.transform.gameObject.CompareTag("Play"))
+			{
+				GameObject switchscene = GameObject.Find("SwitchScene");
+				switchscene.GetComponent<SwitchScene>().WhenPlayButtonPressed();
+			}
+
+		}
+	}
+}
---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SwitchScene : UnityEngine.MonoBehaviour
+{
+	public void WhenPlayButtonPressed()
+	{
+		SceneManager.LoadScene("Game");
+	}
+
+}
